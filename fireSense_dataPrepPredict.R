f12f410ccd53130c6116c1aab4d3809c7363ef38
Ian Eddy <ianmseddy@gmail.com>
2021-02-02 14:56:20 -0800

    fleshing out events

--- fireSense_dataPrepPredict.R
+++ fireSense_dataPrepPredict.R
@@ -15,8 +15,9 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = deparse(list("README.txt", "fireSense_dataPrepPredict.Rmd")),
-  reqdPkgs = list(),
+  reqdPkgs = list("data.table", "PredictiveEcology/fireSenseUtils@development (>=0.0.4.9007)", "raster"),
   parameters = rbind(
+    defineParameter(name = "fireTimeStep", "numeric", 1, NA, NA, )
     defineParameter(name = "whichModulesToPrepare", class = "character",
                     default = c("fireSense_SpreadPredict", "fireSense_IgnitionPredict", "fireSense_EscapeFit"),
                     NA, NA, desc = "Which fireSense fit modules to prep? defaults to all 3")
@@ -53,8 +54,10 @@ defineModule(sim, list(
                  desc = paste("list of projected climate variables in raster stack form",
                               "named according to variable, with names of individual raster layers",
                               "following the convention 'year<year>'")),
+    expectsInput(objectName = "landcoverDT", objectClass = "data.table",
+                 desc = "data.table with pixelID and relevant landcover classes")
     expectsInput(objectName = "terrainDT", objectClass = "data.table",
-                 desc = "data.table with pixelID and relevant terrain variables used by predict models"),
+                 desc = "data.table with pixelID and relevant terrain variables"),
     expectsInput(objectName = "vegComponentsToUse", objectClass = "character",
                  desc = "names of the veg components to use in ignition, escape, and spread predict models")
   ),
@@ -106,13 +109,18 @@ doEvent.fireSense_dataPrepPredict = function(sim, eventTime, eventType) {
 
     prepIgnitionPredictData = {
       sim <- prepare_IgnitionPredict(sim)
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$fireTimeStep,
+                           "fireSense_dataPrepPredict", "prepIgnitionPredictData")
     },
     prepEscapePredictData = {
       sim <- prepare_EscapePredict(sim)
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$fireTimeStep,
+                           "fireSense_dataPrepPredict", "prepEscapePredictData")
     },
     prepSpreadPredictData = {
-      sim <- prepare_SpreadPredict(sim)
-
+      sim <- prepare_SpreadPredict(sim) #see what is specific, maybe don't pass sim
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$fireTimeStep,
+                           "fireSense_dataPrepPredict", "prepSpreadPredictData")
     },
     warning(paste("Undefined event type: \'", current(sim)[1, "eventType", with = FALSE],
                   "\' in module \'", current(sim)[1, "moduleName", with = FALSE], "\'", sep = ""))
@@ -126,6 +134,7 @@ doEvent.fireSense_dataPrepPredict = function(sim, eventTime, eventType) {
 ### template initialization
 Init <- function(sim) {
   # # ! ----- EDIT BELOW ----- ! #
+ #what has to happen in the init? everthing should be dynamic, so... likeyl nothing
 
   # ! ----- STOP EDITING ----- ! #
 
@@ -153,7 +162,12 @@ plotFun <- function(sim) {
 }
 
 ### template for your event1
-Event1 <- function(sim) {
+prepare_spreadPredict <- function(sim) {
+  browser()
+  #cohortData, pixelGroupMap, nonforest_standAge, terrainDT, landcoverDT, PCA....
+  #1) build fireSense vegData from cohortData + landcoverDT
+  terrainAndLCC <- sim$landcoverDT[sim$terrainDT, on = "pixelIndex"]
+
 
 
   return(invisible(sim))
@@ -168,16 +182,29 @@ Event2 <- function(sim) {
 
 .inputObjects <- function(sim) {
 
-
-  #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
+  cacheTags <- c(currentModule(sim), "function:.inputObjects")
   dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
   message(currentModule(sim), ": using dataPath '", dPath, "'.")
 
+  if (!suppliedElsewhere("landcoverDT", sim)) {
+    stop("Please supply this object by running fireSense_dataPrepFit")
+  }
 
   if (!suppliedElsewhere("nonForest_timeSinceDisturbance", sim)) {
-
-    #this is a complicated object to create from scratch. Requires nonforest lcc, lcc raster, firePolys...
-    sim$nonForest_timeSinceDisturbance <-
+    stop("Please supply this object by running fireSense_dataPrepFit")
+    #It is a lot of work to supply some of these, fraught with assumptions
+    #initial TSD will be derived from 1995-2010 fires
+    # firePolys <- Cache(fireSenseUtils::getFirePolygons,
+    #                    fireSenseUtils, years = 1995:2010,
+    #                    studyArea = sim$studyArea,
+    #                    destinationPath = dPath,
+    #                    useInnerCache = FALSE,
+    #                    usertags = c("firePolys", cacheTags))
+    # standAgeMap <- setValues(sim$rasterToMatch, 16) #defaults to not young
+    # #this is a complicated object to create from scratch. Requires nonforest lcc, lcc raster, firePolys...
+    # sim$nonForest_timeSinceDisturbance <- makeTSD(year = 2011, firePolys = firePolys,
+    #                                               standAgeMap = standAgeMap,
+    #                                               lcc = sim$landcoverDT)
   }
 
   return(invisible(sim))
